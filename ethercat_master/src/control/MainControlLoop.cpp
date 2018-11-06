9cb9d06902700d94bc08d8e11433458f3c4166ae
Slava <vtretyakov@synapticon.com>
2018-11-06 18:15:14 +0100

    add dummy (String) ROS publishers and subscribers into MainControlLoop

--- ethercat_master/src/control/MainControlLoop.cpp
+++ ethercat_master/src/control/MainControlLoop.cpp
@@ -3,14 +3,22 @@
 */
 
 #include "MainControlLoop.h"
+#include <stdio.h>
 
 using namespace mcx;
 
 void MainControlLoop::create_(const char *name, parameter_server::Parameter *parameter_server, uint64_t dt_micro_s) {
-
+    sub_ = nh_.subscribe<std_msgs::String>("/command", 10, &MainControlLoop::controlCallback, this);
+    pub_  = nh_.advertise<std_msgs::String>("/feedback", 1);
+    count_ = 0;
 }
 
 bool MainControlLoop::initPhase1_() {
+
+    addParameter("inputVelocity", mcx::parameter_server::ParameterType::INPUT, &input_velocity_);
+    addParameter("outputVelocity", mcx::parameter_server::ParameterType::OUTPUT, &output_velocity_);
+    addParameter("gain", mcx::parameter_server::ParameterType::PARAMETER, &gain_);
+
     return true;
 }
 
@@ -27,5 +35,22 @@ bool MainControlLoop::stopOp_() {
 }
 
 bool MainControlLoop::iterateOp_(const container::TaskTime &system_time, container::UserTime *user_time) {
+
+    output_velocity_ = input_velocity_ *gain_;
+
+    std_msgs::String msg;
+    std::stringstream ss;
+    ss << "running " << count_;
+    msg.data = ss.str();
+
+    pub_.publish(msg);
+    count_++;
+
+    ros::spinOnce();
+
     return true;
+}
+
+void MainControlLoop::controlCallback(const std_msgs::String::ConstPtr& command_msg) {
+    std::cout << command_msg->data << std::endl;
 }
--- ethercat_master/src/control/MainControlLoop.h
+++ ethercat_master/src/control/MainControlLoop.h
@@ -6,6 +6,8 @@
 #define MAINCONTROLLOOP_H
 
 #include <mcx/mcx_core.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
 
 class MainControlLoop : public mcx::container::Module {
 public:
@@ -13,6 +15,8 @@ public:
     MainControlLoop() = default;
 
     ~MainControlLoop() override = default;
+
+    void controlCallback(const std_msgs::String::ConstPtr& command_msg);
 
 private:
     void create_(const char *name, mcx::parameter_server::Parameter *parameter_server, uint64_t dt_micro_s) override;
@@ -26,6 +30,17 @@ private:
     bool stopOp_() override;
 
     bool iterateOp_(const mcx::container::TaskTime &system_time, mcx::container::UserTime *user_time) override;
+
+
+    double input_velocity_;
+    double output_velocity_;
+    double gain_;
+
+    ros::NodeHandle nh_;
+    ros::Subscriber sub_;
+    ros::Publisher pub_;
+    int count_;
+
 };
 
 #endif /* MAINCONTROLLOOP_H */
